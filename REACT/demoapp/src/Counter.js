7b19fbc9dde459837763400eece35aca10167eec
parasf <email@youremail.com>
2021-08-01 13:24:10 +0530

    counter updated

--- REACT/demoapp/src/Counter.js
+++ REACT/demoapp/src/Counter.js
@@ -9,13 +9,27 @@ class Counter extends Component{
         };
     }
     //Increment function
-    increment = () => {
+    increment = (inc) => {
         this.setState(
             {
-                countval: this.state.countval + 1
+                countval: this.state.countval + inc
             }
         );
     }
+    /* 
+    Sample increment function without using arrow function
+    increment(){
+    this.setState({countval:this.state.countval +1})
+    }
+    Inside Constructor:
+    constructor(){
+    super();
+    this.state={
+    countval:0,
+    }
+    this.increment=this.increment.bind(this);
+    }
+    */
      //Decrement function
     decrement = () => {
         this.setState(
@@ -38,7 +52,7 @@ class Counter extends Component{
                 <p>Hello {this.props.name} !</p>
                 <p>WELCOME to counter !..</p>
                 <p>Count:{this.state.countval}</p>
-                <button onClick={this.increment}>Increment</button>
+                <button onClick={() =>  this.increment(4) }>Increment</button>//To send parameters to function 
                 <button onClick={this.decrement}>Decrement</button>
                 <button onClick={this.reset}>Reset</button>
         
